c0233797c7fb0425ad8d81bdaf03fcbc33113cb4
Aadi639707 <aadi639707@gmail.com>
2026-01-20 14:12:28 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+API_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+GITHUB_TOKEN = 'YOUR_GITHUB_PERSONAL_ACCESS_TOKEN'
+
+# Aapki multiple Render API Keys
+RENDER_KEYS = [
+    "rnd_xxxx1", # Pehli ID (10 slots)
+    "rnd_xxxx2", # Dusri ID (10 slots)
+]
